c8ad63c611af259e999f6f790b894e73dfce9f4e
Gabriel Cunha <gabriel_mcunha@hotmail.com>
2018-05-25 11:01:01 -0300

    Add folder view and config route

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
-router.get('/', (req, res, next) => {
-    res.render('index.html');
+router.get('/tasks', (req, res, next) => {
+    res.send('Router Task');
 });
 
 module.exports = router;
