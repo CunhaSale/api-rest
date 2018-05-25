4b3ab7a90e628191d50951b41ad161dd74dc75a4
Gabriel Cunha <gabriel_mcunha@hotmail.com>
2018-05-25 10:22:26 -0300

    Add package.json, .gitignore, Folder src, file routes/index index, config routes/index.js

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const cors = require('cors');
+const express = require('express');
+const app = express();
+
+const indexRoutes = require('./routes/index');
+
+//settings
+app.set('port', process.env.PORT || 3000);
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+//routes
+app.use(indexRoutes);
+
+app.listen(app.get('port'), () => {
+    console.log('server on port', app.get('port'));
+});
